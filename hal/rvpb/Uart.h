c76525ac8c505c2c4dbec71404bbabb27be7e6a2
kyoungjung <kjkim9612@naver.com>
2024-01-09 01:08:21 +0900

    24.1.9 - uart 작성 중

--- hal/rvpb/Uart.h
+++ hal/rvpb/Uart.h
@@ -19,8 +19,16 @@
 
 
 //** 두번째 방법 구조체 사용 ---
+
+#define UART_BASE_ADDRESS0                  0x10009000
+
+//Data Register bits
 typedef union UARTDR_t
 {
+    //union은 메모리를 공유함.
+    //all 이라는 32비트크기 변수와, bits bit field 간 메모리 공유함
+    //만약 bit field의 특정 비트가 변경되면
+    //all 변수의 해당 비트값도 변경됨
     uint32_t all;
     struct
     {
@@ -32,6 +40,49 @@ typedef union UARTDR_t
         uint32_t reserved   :3;     //12:15
     }bits;
 }UARTDR_t;
+
+//Receive Status Register / Error Clear Register bits
+typedef union UARTSR_t
+{
+    uint32_t all;
+    struct 
+    {
+        uint32_t FE         :1;     // 0
+        uint32_t PE         :1;     // 1
+        uint32_t BE         :1;     // 2
+        uint32_t OE         :1;     // 3
+        uint32_t reserved   :28;    // 4 : 31
+    }bits;    
+}UARTSR_t;
+
+//Flag Register bits
+typedef union UARTFR_t
+{
+    uint32_t all;
+    struct
+    {
+        uint32_t CTS        :1;     // 0
+        uint32_t DSR        :1;     // 1
+        uint32_t DCD        :1;     // 2
+        uint32_t BUSY       :1;     // 3
+        uint32_t RXFE       :1;     // 4
+        uint32_t TXFF       :1;     // 5
+        uint32_t RXFF       :1;     // 6
+        uint32_t TXFE       :1;     // 7
+        uint32_t RI         :1;     // 8
+        uint32_t reserved   :23;    // 9 : 31
+    }bits;
+}UARTFR_t;
+
+//IrDA Low-Power Counter Register bits
+typedef union UARTILPR_t
+{
+    uint32_t all;
+    struct
+    {
+
+    }bits;
+}UARTILPR_t;
 
 typedef union UARTCR_t
 {
@@ -55,7 +106,8 @@ typedef union UARTCR_t
     
 }UARTCR_t;
 
-//PL011 
+//PL011
+
 
 
 
